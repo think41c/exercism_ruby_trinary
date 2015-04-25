e3db374ea73043d419668ad368aa27fd6d114f9b
Tony DiNitto <think41c@gmail.com>
2015-04-24 23:49:21 -0400

    Pseduo code written for logic beyond 1 digit.

--- trinary.rb
+++ trinary.rb
@@ -6,8 +6,12 @@ class Trinary
   def to_decimal
     digit_to_check = 0 
     power_to_use   = 0 
+    result         = 0 
 
+    # Digit to check and power to use will use the same number
+    # They will also increment up once at a time in a loop.
     result = @trinary_num[digit_to_check] * 3**power_to_use
+      # result.to_i += result  --- Needs to be keeping a summed total
     result.to_i
   end
 end 
