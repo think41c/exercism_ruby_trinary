b6ea404fd0028fd6e202c34dc73b3e34b6b792ad
Tony DiNitto <think41c@gmail.com>
2015-04-24 23:31:32 -0400

    Initial structure of logic forming for method interaction.

--- trinary.rb
+++ trinary.rb
@@ -1,10 +1,12 @@
 class Trinary
-  def initialize(x)
+  def initialize(trinary_num)
+    @trinary_num = trinary_num
   end
 
   def to_decimal
+    puts "Trinary_num in reverse is #{@trinary_num.reverse}"
     1
   end
-
-
 end 
+
+Trinary.new('1').to_decimal
