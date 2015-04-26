96d83edcf888354f0a3d7e75a7a904fce2460b54
Tony DiNitto <think41c@gmail.com>
2015-04-26 09:07:48 -0400

    Converting trinary digits to integers.

--- trinary.rb
+++ trinary.rb
@@ -6,15 +6,17 @@ class Trinary
 
   def to_decimal
     p "Initial trinary - #{@trinary_num}"
-    digit_to_check = 0 
-    power_to_use   = 0 
-    result         = 0 
-    x              = 0 
+    digit_to_check = 0
+    power_to_use   = 0
+    result         = 0
+    x              = 0
     num_of_digits  = @trinary_num.length
 
     until x == num_of_digits
-      p @trinary_num[x] * 3**x
-      result = @trinary_num[x] * 3**x
+      # p @trinary_num[x] * 3**x
+      trinary_digit = @trinary_num[x].to_i
+      p trinary_digit
+      result = trinary_digit * 3**x
       p "Result = #{result}"
       x += 1
       p "**************"
