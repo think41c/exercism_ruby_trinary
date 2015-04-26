ea8b95406ee76bf8313300b18cef7e69d777a7eb
Tony DiNitto <think41c@gmail.com>
2015-04-25 23:59:33 -0400

    Cleaned up puts statements.

--- trinary.rb
+++ trinary.rb
@@ -10,11 +10,9 @@ class Trinary
     x              = 0 
     num_of_digits  = @trinary_num.length
 
-    # p "num_of_digits = #{num_of_digits} and x = #{x}"
     until x == num_of_digits
       result = @trinary_num[digit_to_check] * 3**power_to_use
         # result.to_i += result  #--- Needs to be keeping a summed total
-      # p "#{result.to_i} <- the multiplied result as an int." 
       x += 1
     end 
     result.to_i
