ff05a6e1a388f044bced28b4ed5f9fee75fab1d4
Tony DiNitto <think41c@gmail.com>
2015-04-26 09:17:20 -0400

    Deleted unused variable declarations.

--- trinary.rb
+++ trinary.rb
@@ -5,14 +5,11 @@ class Trinary
   end
 
   def to_decimal
-    digit_to_check = 0
-    power_to_use   = 0
     result         = 0
     x              = 0
     total          = 0 
-    num_of_digits  = @trinary_num.length
 
-    until x == num_of_digits
+    until x == @num_of_digits
       trinary_digit = @trinary_num[x].to_i
       result = trinary_digit * 3**x
       x += 1
