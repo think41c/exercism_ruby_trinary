e29fe2c9249a45ef19a8a1cd5b909426d372c599
Tony DiNitto <think41c@gmail.com>
2015-04-26 08:53:17 -0400

    Debugging math logic.

--- trinary.rb
+++ trinary.rb
@@ -1,9 +1,10 @@
 class Trinary
   def initialize(trinary_num)
-    @trinary_num = trinary_num
+    @trinary_num = trinary_num.reverse
   end
 
   def to_decimal
+    p "Initial trinary - #{@trinary_num}"
     digit_to_check = 0 
     power_to_use   = 0 
     result         = 0 
@@ -11,12 +12,15 @@ class Trinary
     num_of_digits  = @trinary_num.length
 
     until x == num_of_digits
+      p "x = #{x} , and Trinary = #{@trinary_num}. 3**x = #{3**x}. Trinary[x] = #{@trinary_num[x]}"
+      p @trinary_num[x] * 3**x
       result = @trinary_num[x] * 3**x
-      # result = @trinary_num[digit_to_check] * 3**power_to_use
+      p "Result = #{result}"
       x += 1
+      p "**************"
     end 
     result.to_i
   end
 end 
 
-p Trinary.new('10').to_decimal
+p Trinary.new('19').to_decimal
