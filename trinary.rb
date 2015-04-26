6c13022e584c31782e40b660eeb6f5e266059a4f
Tony DiNitto <think41c@gmail.com>
2015-04-25 23:57:11 -0400

    Logic for looping methods forming.

--- trinary.rb
+++ trinary.rb
@@ -7,13 +7,19 @@ class Trinary
     digit_to_check = 0 
     power_to_use   = 0 
     result         = 0 
+    x              = 0 
+    num_of_digits  = @trinary_num.length
 
-    # Digit to check and power to use will use the same number
-    # They will also increment up once at a time in a loop.
-    result = @trinary_num[digit_to_check] * 3**power_to_use
-      # result.to_i += result  --- Needs to be keeping a summed total
-    result.to_i
+    p "num_of_digits = #{num_of_digits} and x = #{x}"
+    # until x == num_of_digits
+      result = @trinary_num[digit_to_check] * 3**power_to_use
+        # result.to_i += result  #--- Needs to be keeping a summed total
+      p "#{result.to_i} <- the multiplied result as an int." 
+      x += 1
+      result.to_i
+    # end 
   end
 end 
 
-Trinary.new('1').to_decimal
+p Trinary.new('10').to_decimal
+# puts "hi"
