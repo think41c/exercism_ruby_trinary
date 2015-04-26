b1ab17965064205e9cc336541ae2fae46b34437c
Tony DiNitto <think41c@gmail.com>
2015-04-26 08:37:56 -0400

    Loops appropriate amount of times.

--- trinary.rb
+++ trinary.rb
@@ -11,7 +11,8 @@ class Trinary
     num_of_digits  = @trinary_num.length
 
     until x == num_of_digits
-      result = @trinary_num[digit_to_check] * 3**power_to_use
+      result = @trinary_num[x] * 3**x
+      # result = @trinary_num[digit_to_check] * 3**power_to_use
       x += 1
     end 
     result.to_i
