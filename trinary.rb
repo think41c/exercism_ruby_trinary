453fa614cf02216ef63a633f9f57e792c23c1b7b
Tony DiNitto <think41c@gmail.com>
2015-04-25 23:58:58 -0400

    Until loop forming.

--- trinary.rb
+++ trinary.rb
@@ -10,16 +10,15 @@ class Trinary
     x              = 0 
     num_of_digits  = @trinary_num.length
 
-    p "num_of_digits = #{num_of_digits} and x = #{x}"
-    # until x == num_of_digits
+    # p "num_of_digits = #{num_of_digits} and x = #{x}"
+    until x == num_of_digits
       result = @trinary_num[digit_to_check] * 3**power_to_use
         # result.to_i += result  #--- Needs to be keeping a summed total
-      p "#{result.to_i} <- the multiplied result as an int." 
+      # p "#{result.to_i} <- the multiplied result as an int." 
       x += 1
-      result.to_i
-    # end 
+    end 
+    result.to_i
   end
 end 
 
 p Trinary.new('10').to_decimal
-# puts "hi"
